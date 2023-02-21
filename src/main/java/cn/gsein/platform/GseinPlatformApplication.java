3d93d5b776d5b98bec71a1502639c951a190c02d
lhing17 <lhing17@163.com>
2023-02-21 16:28:36 +0800

    init: 初次提交

---
+++ src/main/java/cn/gsein/platform/GseinPlatformApplication.java
@@ -0,0 +1,13 @@
+package cn.gsein.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GseinPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GseinPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/gsein/platform/GseinPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.gsein.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GseinPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

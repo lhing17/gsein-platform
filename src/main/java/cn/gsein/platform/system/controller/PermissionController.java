76d7b7255b6fd34e089620a6de158182ff38ee04
lhing17 <lhing17@163.com>
2023-03-14 14:59:41 +0800

    feat: 角色管理和权限管理相关注解。

--- src/main/java/cn/gsein/platform/system/controller/PermissionController.java
+++ src/main/java/cn/gsein/platform/system/controller/PermissionController.java
@@ -1,13 +1,22 @@
 package cn.gsein.platform.system.controller;
 
-import cn.gsein.platform.system.entity.Result;
 import cn.gsein.platform.system.entity.Permission;
+import cn.gsein.platform.system.entity.Result;
 import cn.gsein.platform.system.service.PermissionService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
+
+/**
+ * 权限管理
+ */
+@Api(value = "/api/v1/permission", tags = {"权限管理"})
 @RestController
 @RequestMapping("/api/v1/permission")
 @Slf4j
@@ -17,23 +26,51 @@ public class PermissionController {
     @Resource
     private PermissionService permissionService;
 
+    /**
+     * 根据ID获取权限
+     */
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "path", dataType = "long", name = "id", value = "权限ID", required = true)
+    })
+    @ApiOperation(value = "根据ID获取权限", notes = "根据ID获取权限", httpMethod = "GET")
     @GetMapping("/{id}")
     Result<Permission> getById(@PathVariable Long id) {
         return Result.ok(permissionService.findById(id));
     }
 
+    /**
+     * 保存权限
+     */
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "body", dataType = "Permission", name = "permission", value = "权限实体", required = true)
+    })
+    @ApiOperation(value = "保存权限", notes = "保存权限", httpMethod = "POST")
     @PostMapping("/save")
     Result<Void> save(@RequestBody Permission permission) {
         permissionService.save(permission);
         return Result.ok();
     }
 
+    /**
+     * 修改权限
+     */
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "body", dataType = "Permission", name = "permission", value = "权限实体", required = true)
+    })
+    @ApiOperation(value = "修改权限", notes = "修改权限", httpMethod = "PUT")
     @PutMapping("/update")
     Result<Void> update(@RequestBody Permission permission) {
         permissionService.updateById(permission);
         return Result.ok();
     }
 
+    /**
+     * 删除权限
+     */
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "path", dataType = "long", name = "id", value = "权限ID", required = true)
+    })
+    @ApiOperation(value = "删除权限", notes = "删除权限", httpMethod = "DELETE")
     @DeleteMapping("/{id}")
     Result<Void> deleteById(@PathVariable Long id) {
         permissionService.deleteById(id);
--- src/main/java/cn/gsein/platform/system/controller/RoleController.java
+++ src/main/java/cn/gsein/platform/system/controller/RoleController.java
@@ -3,11 +3,19 @@ package cn.gsein.platform.system.controller;
 import cn.gsein.platform.system.entity.Result;
 import cn.gsein.platform.system.entity.Role;
 import cn.gsein.platform.system.service.RoleService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
+/**
+ * 角色管理
+ */
+@Api(value = "/api/v1/role", tags = {"角色管理"})
 @RestController
 @RequestMapping("/api/v1/role")
 @Slf4j
@@ -17,23 +25,51 @@ public class RoleController {
     @Resource
     private RoleService roleService;
 
+    /**
+     * 根据ID获取角色
+     */
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "path", dataType = "long", name = "id", value = "角色ID", required = true)
+    })
+    @ApiOperation(value = "根据ID获取角色", notes = "根据ID获取角色", httpMethod = "GET")
     @GetMapping("/{id}")
     Result<Role> getById(@PathVariable Long id) {
         return Result.ok(roleService.findById(id));
     }
 
+    /**
+     * 保存角色
+     */
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "body", dataType = "Role", name = "role", value = "角色实体", required = true)
+    })
+    @ApiOperation(value = "保存角色", notes = "保存角色", httpMethod = "POST")
     @PostMapping("/save")
     Result<Void> save(@RequestBody Role role) {
         roleService.save(role);
         return Result.ok();
     }
 
+    /**
+     * 修改角色
+     */
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "body", dataType = "Role", name = "role", value = "角色实体", required = true)
+    })
+    @ApiOperation(value = "修改角色", notes = "修改角色", httpMethod = "PUT")
     @PutMapping("/update")
     Result<Void> update(@RequestBody Role role) {
         roleService.updateById(role);
         return Result.ok();
     }
 
+    /**
+     * 删除角色
+     */
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "path", dataType = "long", name = "id", value = "角色ID", required = true)
+    })
+    @ApiOperation(value = "删除角色", notes = "删除角色", httpMethod = "DELETE")
     @DeleteMapping("/{id}")
     Result<Void> deleteById(@PathVariable Long id) {
         roleService.deleteById(id);
